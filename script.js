da408daf32920549ec434c9e63bac8620f2079b1
Adereb-web <123363757+Adereb-web@users.noreply.github.com>
2024-01-06 09:48:04 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function sayHello(){
+
+  let name = document.getElementById("name").value;
+
+  let greeting = "Hello, " + name + "!";
+
+  document.getElementById("output").textContent = greeting;
+}
