14ae6146af39427a2a8c129b333cd5f318d954ac
Adereb-web <123363757+Adereb-web@users.noreply.github.com>
2024-04-19 19:30:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ document.addEventListener('DOMContentLoaded', function () {
     // Display personalized greeting
     if (storedName) {
         greetingContainer.textContent = `Hello, ${storedName}!`;
+        clearButton.style.display = 'inline-block'; // Show clear button
     } else {
         greetingContainer.textContent = 'Welcome Back! Please enter your full name.';
+        clearButton.style.display = 'none'; // Hide clear button
     }
 
     // To handle the submit event
@@ -25,6 +27,9 @@ document.addEventListener('DOMContentLoaded', function () {
         // displaying a personalized greeting
         greetingContainer.textContent = `Hello, ${enteredName}! Welcome to my greeting app.`;
         greetingContainer.style.color = 'Purple';
+
+        // Show clear button
+        clearButton.style.display = 'inline-block';
     });
 
     // handling the clear button
@@ -35,5 +40,8 @@ document.addEventListener('DOMContentLoaded', function () {
         // updating the greeting message
         greetingContainer.innerHTML = 'Name Cleared! Please enter another name you will like me to greet.';
         greetingContainer.style.color = 'rgb(179, 170, 162)';
+
+        // Hide clear button
+        clearButton.style.display = 'none';
     });
 });
